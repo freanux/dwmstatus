9b9badfaf77bb954c0b218310164828d4a29d499
freanux <pw@earthwave.ch>
2024-05-12 23:24:34 +0200

    beautify statusbar

--- main.cpp
+++ main.cpp
@@ -7,14 +7,16 @@
 namespace {
 
     const Section sections[] = {
-        { fn_execute_home,      "scripts/dwm/microstatus.sh",                       0, SIGUSR2 },
-        { fn_print,             "^f2^^c#444444^^r0,0,2,38^^f2^^d^^f2^ ",            0, 0       },
-        { fn_execute_home,      "scripts/dwm/audiostatus.sh",                       0, SIGUSR1 },
-        { fn_print,             " ^f2^^c#444444^^r0,0,2,38^^f2^^d^ ",               0, 0       },
-        { fn_datetime,          "%a, %d %b %Y ^f2^^c#444444^^r0,0,2,38^^f2^^d^ %T", 1, 0       },
-        { fn_print,             " ^f2^^c#444444^^r0,0,2,38^^f2^^d^ ",               0, 0       },
-        { fn_kernel_release,    0,                                                  0, 0       },
-        { fn_print,             "^f12^",                                            0, 0       },
+        { fn_print,             "^b#422a49^  ^c#422a49^^r0,7,7,28^^f6^^d^^b#2a2241^ ",         0, 0       },
+        { fn_kernel_release,    0,                                                              0, 0       },
+        { fn_print,             " ^d^ ",                                                        0, 0       },
+        { fn_print,             "^b#422a49^  ^c#422a49^^r0,7,7,28^^f6^^d^^b#2a2241^ ",         0, 0       }, 
+        { fn_datetime,          "%a, %d %b %Y ^f2^^c#444444^^d^ ^b#422a49^  ^b#2a2241^ %T",    1, 0       },
+        { fn_print,             " ^d^",                                                         0, 0       },
+        { fn_execute_home,      "scripts/dwm/microstatus.sh",                                   0, SIGUSR2 },
+        { fn_print,             "^f2^^c#444444^^r0,0,2,42^^f2^^d^^f2^ ",                        0, 0       },
+        { fn_execute_home,      "scripts/dwm/audiostatus.sh",                                   0, SIGUSR1 },
+        { fn_print,             "^f12^",                                                        0, 0       },
         { 0, 0, 0, 0 }
     };
 
